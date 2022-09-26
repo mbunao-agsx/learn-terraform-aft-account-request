22ab866d4fcf50e2528140baa0b26994f6038a0e
mbunao-agsx <mbunao@agsx.net>
2022-09-26 22:08:52 +0800

    Added test account in TestOU

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,38 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "mbunao+sandbox-test-aft@agsx.net"
-    AccountName               = "mbunao-sandbox-test-aft"
-    ManagedOrganizationalUnit = "TestOU"
-    SSOUserEmail              = "mbunao+sandbox-test-aft@agsx.net"
+    AccountEmail              = "mbunao+sandbox-aft@agsx.net"
+    AccountName               = "mbunao-sandbox-aft"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "mbunao+sandbox-aft@agsx.net"
+    SSOUserFirstName          = "Mae"
+    SSOUserLastName           = "Bunao"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform - 20220926"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
+module "test" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "mbunao+test-aft@agsx.net"
+    AccountName               = "mbunao-test-aft"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "mbunao+test-aft@agsx.net"
     SSOUserFirstName          = "Mae"
     SSOUserLastName           = "Bunao"
   }
