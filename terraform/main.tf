dd37e133b46336a6d099e06800f2108bf60b477a
mbunao-agsx <mbunao@agsx.net>
2022-09-29 11:00:07 +0800

    Added mbunao-test-aft

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,3 +53,31 @@ module "amarigza-sb-02" {
 
   account_customizations_name = "sandbox"
 }
+
+module "mbunao-test-aft" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "mbunao+test-aft@agsx.net"
+    AccountName               = "mbunao-test-aft"
+    ManagedOrganizationalUnit = "TestOU (ou-eq2e-wllvkya9)"
+    SSOUserEmail              = "mbunao+test-aft@agsx.net"
+    SSOUserFirstName          = "Mae"
+    SSOUserLastName           = "Bunao"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform - 20220929"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "test"
+}
